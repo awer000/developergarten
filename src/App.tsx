4999d697b5a08ca864f74400ea2d9004e8ad6263
Minwon, Ahn <minwon.ahn@weddingbook.com>
2020-05-16 14:10:46 +0900

    테스트 컴포넌트 앱에서 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import PageHeader from "./components/PageHeader"
+import List from "./components/List"
 
 function App() {
   return (
     <div className='App'>
       <PageHeader logoName='developergarten' />
+      <List />
     </div>
   )
 }
